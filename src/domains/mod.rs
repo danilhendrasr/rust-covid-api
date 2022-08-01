7b72295e99c005e529d3faaf38d54c98de2d360b
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2022-08-01 22:33:37 +0000

    refactor: delete unused domains directory

--- src/domains/mod.rs
+++
@@ -1,5 +0,0 @@
-mod date_constraint_string;
-mod query_params;
-
-pub use date_constraint_string::DateConstraintString;
-pub use query_params::QueryParams;
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 pub mod constants;
-pub mod domains;
 pub mod routes;
 pub mod types;
 pub mod utils;
